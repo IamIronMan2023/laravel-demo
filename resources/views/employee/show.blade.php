36fedeea7bcbf5b3eb7b9f7436396ab7c42c24e2
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-16 20:34:13 +0800

    Add delete confirmation

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -7,14 +7,33 @@
     <h3>Age: {{$employee -> age}}</h3>
     <h3>Email: {{$employee -> email}}</h3>
 
-    <p><a class="btn btn-primary" {{route('employees.index')}}">Back to List</a></p>
+    <p><a class="btn btn-primary" href="{{route('employees.index')}}">Back to List</a></p>
     <p><a class="btn btn-primary" href="{{route('employees.edit', ['employee' => $employee->id])}}">Edit</a></p>
+    <p><button type="button" class="btn btn-danger" data-bs-toggle="modal"
+            data-bs-target="#deleteEmployeeModal">Delete</button></p>
 
-    <form action="{{route('employees.destroy', $employee->id)}}" method="POST">
-        @method('DELETE')
-        @csrf
-
-        <input class="btn btn-danger" type="submit" value="Delete">
-    </form>
 </div>
+
+<div class="modal" tabindex="-1" id="deleteEmployeeModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="{{route('employees.destroy', $employee->id)}}" method="POST">
+                @method('DELETE')
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title">Delete Employee Confirmation</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </div>
+        </div>
+        </form>
+    </div>
+</div>
+
 @endsection
